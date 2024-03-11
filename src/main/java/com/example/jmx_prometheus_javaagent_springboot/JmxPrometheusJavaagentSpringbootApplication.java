e5ef2553565e92a4dca78469a0c57b1ccf667f16
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2024-03-11 22:36:52 +0800

    创建项目

---
+++ src/main/java/com/example/jmx_prometheus_javaagent_springboot/JmxPrometheusJavaagentSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmx_prometheus_javaagent_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmxPrometheusJavaagentSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmxPrometheusJavaagentSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jmx_prometheus_javaagent_springboot/JmxPrometheusJavaagentSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmx_prometheus_javaagent_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmxPrometheusJavaagentSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
